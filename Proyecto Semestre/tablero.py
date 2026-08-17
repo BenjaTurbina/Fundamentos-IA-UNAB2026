f59f914a234a5023d10cc267ac8f7e0278a2a07d
BenjaTurbina <b.bravolpez@uandresbello.edu>
2026-08-16 21:38:04 -0400

    "Creacion de tablero NxN"

--- Proyecto Semestre/tablero.py
+++ Proyecto Semestre/tablero.py
@@ -1,44 +1,38 @@
-#def MostrarTablero(n):
-#    print(\t )
-#    for i in range (n):
-#        print(i)
+# Funcion que verifica que se ingresen solo numeros
+def verificar_solo_digito(digito):
+    while not isinstance(digito, int):
+        try:
+            digito = int(digito)
+        except ValueError:
+            digito = input("Caracter no valido, ingrese nuevamente: ")
+    return digito
 
-        
 
-
-n = int(input("Ingrese la cantidad de casillas: "))
-
+n = verificar_solo_digito(input("Ingrese un numero de casillas: "))
 while n < 6 or n > 12:
     print(f" {n} fuera del rango permitido de casillas")
-    n = int(input("Ingrese la cantidad de casillas: "))
+    n = verificar_solo_digito(input("Ingrese la cantidad de casillas: "))
 
-tablero = [["." for j in range(n)] for i in range(n)]
+# Creacion de matriz NxN con caracteres "*" en casa casilla
+tablero = [["*" for j in range(n)] for i in range(n)]
 
-#for i in range(2):
-    #for j in range(n):
-       # tablero[i][j] = "B"
+# Verificacion de la creacion correcta con el tablero 
+for i in tablero:
+    print(" | ".join(i))
 
-#for i in range(n - 2, n):
-    #for j in range(n):
-        #tablero[i][j] = "N"
-
-#for fila in tablero:
-    #print(*fila)
+mitad_tablero = n//2 
 
 
-m=0
-for i in range(n):
-    tablero = [["."for j in range(n)]
-               for i in range(n)]
-    if m < 2 or m > n-2:
-        while m<2:
-            for j in range (n):
-                tablero[i][j] = "B"
-        while m>n-2:            
-            for j in range (n):
-                tablero[i][j] = "N"   
-    m = m + 1
+print("-----------------------------------------------")    
 
+# Tablero modificado con las fichas negras y blancas en su posicion
+for i in range(2):
+    for j in range(n):
+        tablero[i][j] = "B"
+for i in range(n - 2, n):
+    for j in range(n):
+        tablero[i][j] = "N"
 
-for fila in tablero:
-    print(" | ".join(fila))
+for i in tablero:
+    print(" | ".join(i))
+
