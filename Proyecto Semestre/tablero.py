5280742509d7446335aaa9ffdd61073862092479
BenjaTurbina <b.bravolpez@uandresbello.edu>
2026-08-17 21:18:18 -0400

    Creacion de seccion de turnos basicos en tablero.py

--- Proyecto Semestre/tablero.py
+++ Proyecto Semestre/tablero.py
@@ -1,3 +1,6 @@
+#Importacion de la libreria numpy para la creacion de la matriz de juego
+import numpy as np
+
 # Funcion que verifica que se ingresen solo numeros
 def verificar_solo_digito(digito):
     while not isinstance(digito, int):
@@ -14,16 +17,9 @@ while n < 6 or n > 12:
     n = verificar_solo_digito(input("Ingrese la cantidad de casillas: "))
 
 # Creacion de matriz NxN con caracteres "*" en casa casilla
-tablero = [["*" for j in range(n)] for i in range(n)]
-
-# Verificacion de la creacion correcta con el tablero 
-for i in tablero:
-    print(" | ".join(i))
-
-mitad_tablero = n//2 
+tablero = np.full((n,n),'*')
 
 
-print("-----------------------------------------------")    
 
 # Tablero modificado con las fichas negras y blancas en su posicion
 for i in range(2):
@@ -33,6 +29,48 @@ for i in range(n - 2, n):
     for j in range(n):
         tablero[i][j] = "N"
 
-for i in tablero:
-    print(" | ".join(i))
 
+turno = 0
+while True:
+    bandera_negra = False
+    bandera_blanca = False
+    turno += 1
+    if turno % 2 == 1:
+        while not bandera_negra:
+            print(f"---------------- Turno N*{turno}: Fichas Negras ----------------")
+            print(tablero)
+            print("Seleccione una ficha")
+            # Crear una condicion donde se coloquen cordenadas validas dentro de la matriz           
+            mov_ficha_fila = verificar_solo_digito(input("Ingrese fila: "))
+            mov_ficha_columa =  verificar_solo_digito(input("Ingrese columna: "))
+            print(f"Cordenadas ficha seleccionada: {mov_ficha_fila},{mov_ficha_columa}")
+            valor_encontrado = tablero[mov_ficha_fila][mov_ficha_columa]
+            if valor_encontrado == "N":
+                print(f"Ficha Negra: {mov_ficha_fila},{mov_ficha_columa} seleccionada")
+                bandera_negra = True
+            # Colocar una opcion si la ficha no tiene capacidad de moviento (En turno 1 solamente se podrian mover las fichas de delante)
+
+            # Si el valor no corresponde algo dentro de la matriz muestra
+            else:
+                print("Ficha seleccionada no valida para el turno!")
+        print("Has salido del ciclo while al seleccionar la correcta!!")
+
+    else:
+        print(f"---------------- Turno N*{turno}: Fichas Blancas ----------------")
+        while not bandera_blanca:   
+            print(tablero)
+            print("Seleccione una ficha")           
+            mov_ficha_fila = verificar_solo_digito(input("Ingrese fila: "))
+            mov_ficha_columa =  verificar_solo_digito(input("Ingrese columna: "))
+            # Crear una condicion donde se coloquen cordenadas validas dentro de la matriz
+            print(f"Cordenadas ficha seleccionada: {mov_ficha_fila},{mov_ficha_columa}")
+            valor_encontrado = tablero[mov_ficha_fila][mov_ficha_columa]
+            if valor_encontrado == "B":
+                print(f"Ficha Blanca: {mov_ficha_fila},{mov_ficha_columa} seleccionada")
+                bandera_blanca = True
+            # Colocar una opcion si la ficha no tiene capacidad de moviento (En turno 1 solamente se podrian mover las fichas de delante)
+
+            # Si el valor no corresponde algo dentro de la matriz muestra
+            else:
+                print("Ficha seleccionada no valida para el turno!")
+        print("Has salido del ciclo while al seleccionar la correcta!!")
